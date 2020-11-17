8a332f36e75f81ac98092dd930c6fbe419c93fe0
Kamil Duliniec <123dulko@wp.pl>
2020-11-17 23:57:58 +0100

    complete day 2 and 3

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,99 @@
-// this is your main.js script
+const nameChallenge = "30 Days Of JavaScript";
+const nameCompany = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+const because =
+  "You cannot end a sentence with because because because is a conjunction";
+const love =
+  "Love is the best thing in this world. Some found their love and some are still looking for their love.";
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+const annual =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month";
+
+//lvl 1
+
+console.log(nameChallenge);
+console.log(nameChallenge.length);
+console.log(nameChallenge.toUpperCase());
+console.log(nameChallenge.toLowerCase());
+console.log(nameChallenge.substr(0, 2));
+console.log(nameChallenge.substring(0, 2));
+console.log(nameChallenge.substring(3));
+console.log(nameChallenge.includes("Script"));
+console.log(nameChallenge.split());
+console.log(nameChallenge.split(" "));
+console.log(nameCompany.split(","));
+console.log(nameChallenge.replace("JavaScript", "Python"));
+console.log(nameChallenge.charAt(15));
+console.log(nameChallenge.charCodeAt("J"));
+console.log(nameChallenge.indexOf("a"));
+console.log(nameChallenge.lastIndexOf("a"));
+console.log(because.indexOf("because"));
+console.log(because.lastIndexOf("because"));
+console.log(because.search("because"));
+console.log(nameChallenge.trim());
+console.log(nameChallenge.startsWith(30));
+console.log(nameChallenge.endsWith("t"));
+console.log(nameChallenge.match(/a/g));
+console.log(nameChallenge.concat(" - 30 Days Of JavaScript"));
+console.log(nameChallenge.repeat(2));
+
+//lvl2
+
+console.log(
+  'The quote "There is no exercise better for the heart than reaching \ndown and lifting people up." by John Holmes teaches us to help one \nanother.'
+);
+
+console.log(
+  "Love is not patronizing and charity isn't about pity, it is about \nlove. Charity and love are the same -- with charity you give love, so \ndon't just give money but reach out your hand instead."
+);
+
+const number1 = "10";
+console.log(number1 === 10);
+const number2 = parseInt(number1);
+console.log(number2 === 10);
+
+const number3 = "9.8";
+console.log(parseFloat(number3));
+console.log(number3 === 10);
+number4 = Math.ceil(number3);
+console.log(number4 === 10);
+
+if ("python".includes("on") && "jargon".includes("on")) {
+  console.log("yes Sir");
+}
+
+console.log("I hope this course is not full of jargon".includes("jargon"));
+
+console.log(Math.random() * 101);
+console.log(Math.random() * (101 - 50) + 50);
+console.log(Math.random() * 256);
+
+const name = "javascript";
+console.log(name[Math.floor(Math.random() * name.length)]);
+
+console.log("1 1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125");
+
+console.log(because.substr(31, 23));
+
+//lvl3
+
+console.log(love.match(/love/g));
+console.log(because.match(/because/g));
+
+console.log(sentence.replace(/[^\w\s]/gi, ""));
+
+const regex = /\d+/g;
+
+const allNumbers = annual.match(regex);
+
+const convert = allNumbers.map(function (item) {
+  return parseInt(item, 10);
+});
+
+const summarize = convert.reduce((sum,res) => {
+  return sum + res;
+});
+
+console.log(convert);
+
+console.log(summarize);
--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,137 @@
-// this is your main.js script
+//lvl1
+
+const firstName = "Pablo";
+const lastName = "Roletsky";
+const country = "Grenland";
+const city = "North Pole";
+const age = 33;
+const isMarried = false;
+const year = 2020;
+const assign = 15000;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+console.log(typeof assign);
+
+console.log("10" == 10);
+console.log(parseInt("9.8") == 10);
+
+console.log("10" == 10);
+console.log(0 == false);
+console.log(1 == true);
+
+console.log(1 === true);
+console.log(!2 > 0 == true);
+console.log(2 === "2");
+
+//5
+true;
+true;
+false;
+false;
+true;
+true;
+false;
+false;
+false;
+true;
+false;
+console.log("python".length !== "jargon".length);
+
+//6
+true;
+false;
+true;
+true;
+false;
+true;
+true;
+false;
+true;
+true;
+
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth() + 1);
+console.log(new Date());
+console.log(new Date().getDate());
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(new Date().getTime());
+
+//lvl2
+
+const base = prompt("enter base of triangle", 10);
+const height = prompt("enter height of triangle", 20);
+const area = 0.5 * base * height;
+console.log(area);
+
+const a = Number(prompt("enter side a", 5));
+const b = Number(prompt("enter side b", 4));
+const c = Number(prompt("enter side c", 3));
+const perimeter = a + b + c;
+console.log(perimeter);
+
+const z = Number(prompt("enter length", 20));
+const x = Number(prompt("enter width", 5));
+const area2 = z * x;
+console.log(area2);
+console.log((perimeter2 = 2 * (z + x)));
+
+// 4,5,6,7,8,9 => only calculate, no programming - I skipped it
+
+const hours = Number(prompt("enter a hours", 40));
+const ratePerHour = Number(prompt("enter a rate per hours", 10));
+const payment = hours * ratePerHour + "$ per week ";
+console.log(payment);
+
+const sayName = Number(prompt("what's your name?"));
+
+sayName > 7
+  ? console.log("your name is long")
+  : console.log("your name is short");
+
+const name2 = "Pablo";
+const surname2 = "Roletsky";
+
+name2.length > surname2.length
+  ? console.log(
+      `Your first name, ${name2} is longer than your family name, ${surname2}`
+    )
+  : console.log(
+      `Your first name, ${name2} is shorten than your family name, ${surname2}`
+    );
+
+let myAge = 250;
+let yourAge = 25;
+
+console.log(`I am ${myAge - yourAge} years older than you.`);
+
+const userAge = Number(prompt("enter your age"));
+
+userAge >= 18
+  ? console.log(`You are ${userAge}. You are old enough to drive`)
+  : console.log(
+      `You are ${userAge}. You will be allowed to drive after ${18 - userAge}`
+    );
+
+const howOldAreYou = Number(prompt("enter number of years"));
+
+console.log(`you lived: ${howOldAreYou * 365 * 24 * 60 * 60} seconds`);
+
+const timeZone = new Date().toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+console.log(
+  new Date().toLocaleString("pl-PL", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  }) 
+);
